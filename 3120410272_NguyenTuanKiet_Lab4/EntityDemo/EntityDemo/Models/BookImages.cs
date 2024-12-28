68480a039c90b5fefc20b815790bab5daddc21b1
MarkDev2002 <nguyentuankiet1309work@gmail.com>
2024-12-28 10:54:52 +0700

    All

---
+++ 3120410272_NguyenTuanKiet_Lab4/EntityDemo/EntityDemo/Models/BookImages.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityDemo.Models
+{
+    public class BookImages
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BookId { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
---
+++ 3120410272_NguyenTuanKiet_Lab4/EntityDemo/EntityDemo/Models/Catalogue.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityDemo.Models
+{
+    public class Catalogue
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public ICollection<BookCatalogue> BookCatalogues { get; set; }
+    }
+}
